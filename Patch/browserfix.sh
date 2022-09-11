49c7c0dc8f56a99c846e8aecce3574fc4c1eaddd
wahasa <69626847+wahasa@users.noreply.github.com>
2022-09-11 13:47:33 +0700

    Add files via upload

---
+++ Patch/browserfix.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd /etc/apt
+cp sources.list /root
+cd
+
+#echo "Adding Debian repo for Chromium installation"
+echo "deb http://ftp.debian.org/debian buster main
+deb http://ftp.debian.org/debian buster-updates main" >> /etc/apt/sources.list
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys DCC9EFBF77E11517
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AA8E81B4331F7F50
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 112695A0E562B32A
+
+apt update && apt upgrade -y
+sudo apt install firefox-esr -y
+#sudo apt install chromium -y
+#sed -i 's/chromium %U/chromium --no-sandbox --test-type %U/g' /usr/share/applications/chromium.desktop
+
+rm -rf /etc/apt/sources.list
+mv sources.list /etc/apt/
---
+++ Patch/libreofficefix.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt install libreoffice
+rm -rf /usr/lib/libreoffice/program/oosplash
+
+#wget https://raw.githubusercontent.com/tuanpham-dev/termux-ubuntu/master/oosplash?raw=true  -q -O /usr/lib/libreoffice/program/oosplash
+
+chmod +x /usr/lib/libreoffice/program/oosplash
+mkdir /prod && mkdir /prod/version
+#echo "Patch has been applied successfully"
