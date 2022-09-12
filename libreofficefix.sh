f674e0da8cb3b888529f186941ea08c12cbc60a6
wahasa <69626847+wahasa@users.noreply.github.com>
2022-09-12 09:59:13 +0700

    Update libreofficefix.sh

--- libreofficefix.sh
+++ libreofficefix.sh
@@ -2,7 +2,7 @@
 sudo apt install libreoffice
 rm -rf /usr/lib/libreoffice/program/oosplash
 
-wget -q https://raw.githubusercontent.com/wahasa/Ubuntu/main/Patch/oosplash?raw=true -O /usr/lib/libreoffice/program/oosplash
+wget -q https://github.com/wahasa/Ubuntu/blob/main/Patch/oosplash?raw=true -O /usr/lib/libreoffice/program/oosplash
 
 chmod +x /usr/lib/libreoffice/program/oosplash
 mkdir /prod && mkdir /prod/version
