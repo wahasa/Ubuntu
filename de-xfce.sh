f76e608363b7b93cd647e7b046ceb56884bc06c3
wahasa <69626847+wahasa@users.noreply.github.com>
2022-09-12 09:44:42 +0700

    Update de-xfce.sh

--- de-xfce.sh
+++ de-xfce.sh
@@ -1,37 +1,30 @@
 #!/bin/bash
-
 #Get the necessary components
 apt-get update
 apt-get install udisks2 -y
-echo  " "  > /var/lib/dpkg/info/udisks2.postinst
-dpkg --configure -a
-apt-mark hold udisks2
-apt-get install keyboard-configuration -y
-apt-get install tzdata -y
-apt-get install sudo -y
+echo  " "  > /var/lib/dpkg/info/udisks2.postinst                  apt-mark hold udisks2
+apt-get install keyboard-configuration -y                         apt-get install tzdata -y                                         apt-get install sudo -y
 apt-get install xfce4 xfce4-goodies xfce4-terminal -y
 apt-get install tigervnc-standalone-server dbus-x11 -y
-apt-get --fix-broken install
-apt-get clean
+ln -s /dev/null /etc/tmpfiles.d/journal-nocow.conf
+export USER=root                                                  apt-get --fix-broken install                                      apt-get clean
 
 #Setup the necessary files
 mkdir -p ~/.vnc
 echo "#!/bin/bash
 export PULSE_SERVER=127.0.0.1
 xrdb $HOME/.Xresources
-startxfce4" > ~/.vnc/xstartup
-echo "vncserver -geometry 1600x900 -name remote-desktop :1 -xstartup" > /usr/local/bin/vnc-start
+startxfce4" > ~/.vnc/xstartup                                     echo "vncserver -geometry 1600x900 -name remote-desktop :1 -xstar>
 echo "vncserver -kill :1" > /usr/local/bin/vnc-stop
-
+clear
 chmod +x ~/.vnc/xstartup
 chmod +x /usr/local/bin/vnc-start
 chmod +x /usr/local/bin/vnc-stop
-clear
 
+echo " "
 echo "Installing browser,.."
 echo " "
-wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/browserfix.sh && chmod +x browserfix.sh && ./browserfix.sh
-clear
+wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/Patch/b>clear
 echo " "
 echo "Vnc Server address will run at 127.0.0.1:5901"
 echo " "
