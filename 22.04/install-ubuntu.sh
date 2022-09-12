f8719379187e7a64e42f68e5c991723ce50ecb85
wahasa <69626847+wahasa@users.noreply.github.com>
2022-09-12 18:30:16 +0700

    Update install-ubuntu.sh

--- 22.04/install-ubuntu.sh
+++ 22.04/install-ubuntu.sh
@@ -27,7 +27,7 @@ if [ "$first" != 1 ];then
                 *)
                         echo "unknown architecture"; exit 1 ;;
                 esac
-                wget "" -O $tarball
+                wget "https://partner-images.canonical.com/core/jammy/current/ubuntu-jammy-core-cloudimg-${archurl}-root.tar.gz" -O $tarball
         fi
         cur=`pwd`
         mkdir -p "$folder"
