0a50986c54ddd664456d2f344ec756e915bf8f22
wahasa <69626847+wahasa@users.noreply.github.com>
2022-09-13 11:12:32 +0700

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -112,5 +112,5 @@ echo "You can now start Ubuntu with 'ubuntu' script next time"
 echo " "
 rm -rf ~/.bash_profile" > $folder/root/.bash_profile
    bash $bin
-   #rm install-ubuntu.sh
+   rm install-ubuntu.sh
    rm audiofix.sh
