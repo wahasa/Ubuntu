a3576b046e301faa65c4b8aa797ab0655f90b212
wahasa <69626847+wahasa@users.noreply.github.com>
2022-09-13 11:19:02 +0700

    Update and rename de-kde.sh to Desktop/de-kde.sh

--- Desktop/de-kde.sh
+++ Desktop/de-kde.sh
@@ -4,13 +4,14 @@
 apt-get update
 apt-get install udisks2 -y
 echo  " "  > /var/lib/dpkg/info/udisks2.postinst
-dpkg --configure -a
 apt-mark hold udisks2
 apt-get install keyboard-configuration -y
 apt-get install tzdata -y
 apt-get install sudo -y
-apt-get install kde-standard kde-plasma-desktop konsole -y
+apt-get install kde-standard konsole -y
 apt-get install tigervnc-standalone-server dbus-x11 -y
+ln -s /dev/null /etc/tmpfiles.d/journal-nocow.conf
+export USER=root
 apt-get --fix-broken install
 apt-get clean
 
@@ -22,15 +23,15 @@ xrdb $HOME/.Xresources
 startplasma-x11" > ~/.vnc/xstartup
 echo "vncserver -geometry 1600x900 -name remote-desktop :1 -xstartup" > /usr/local/bin/vnc-start
 echo "vncserver -kill :1" > /usr/local/bin/vnc-stop
-
+clear
 chmod +x ~/.vnc/xstartup
 chmod +x /usr/local/bin/vnc-start
 chmod +x /usr/local/bin/vnc-stop
-clear
 
+echo " "
 echo "Installing browser,.."
 echo " "
-#wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/browserfix.sh && chmod +x browserfix.sh && ./browserfix.sh
+wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/Patch/browserfix.sh && chmod +x browserfix.sh && ./browserfix.sh
 clear
 echo " "
 echo "Vnc Server address will run at 127.0.0.1:5901"
