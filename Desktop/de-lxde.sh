6b00475174c5e8d2c811c1374dd489968dd4389a
wahasa <69626847+wahasa@users.noreply.github.com>
2022-09-13 08:20:08 +0700

    Update and rename de-lxde.sh to Desktop/de-lxde.sh

--- Desktop/de-lxde.sh
+++ Desktop/de-lxde.sh
@@ -8,7 +8,7 @@ apt-mark hold udisks2
 apt-get install keyboard-configuration -y
 apt-get install tzdata -y
 apt-get install sudo -y
-apt-get install lxde lxde-common lxterminal -y
+apt-get install lxde lxterminal -y
 apt-get install tigervnc-standalone-server dbus-x11 -y
 ln -s /dev/null /etc/tmpfiles.d/journal-nocow.conf
 mv /usr/bin/lxpolkit /usr/bin/lxpolkit.bak
