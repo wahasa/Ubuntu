cc9f5b80aa0eab8f7a2bbab46795f2e120e467e8
wahasa <69626847+wahasa@users.noreply.github.com>
2022-09-13 12:13:22 +0700

    Update firefox-esr.sh

--- firefox-esr.sh
+++ firefox-esr.sh
@@ -12,7 +12,7 @@ deb http://ftp.debian.org/debian bullseye-updates main" >> /etc/apt/sources.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 605c66f00d6c9793
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 73a4f27b8dd47936
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys a48449044aad5c5d
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys dc30d7c23cbbabee
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4dfab270caa96dfa
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys eda0d2388ae22ba9
 
 apt update && apt upgrade -y
