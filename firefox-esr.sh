d27482476de40cbcd88e4993ec33e67f98a6ffe6
wahasa <69626847+wahasa@users.noreply.github.com>
2022-09-13 09:17:08 +0700

    Update firefox-esr.sh

--- firefox-esr.sh
+++ firefox-esr.sh
@@ -6,13 +6,14 @@ cd
 
 #echo "Adding Debian repo for Chromium installation"
 
-echo "deb http://ftp.debian.org/debian buster main
-deb http://ftp.debian.org/debian buster-updates main" >> /etc/apt/sources.list
+echo "deb http://ftp.debian.org/debian bullseye main
+deb http://ftp.debian.org/debian bullseye-updates main" >> /etc/apt/sources.list
 
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys DCC9EFBF77E11517
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AA8E81B4331F7F50
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 112695A0E562B32A
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 605c66f00d6c9793
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 73a4f27b8dd47936
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys a48449044aad5c5d
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys dc30d7c23cbbabee
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys eda0d2388ae22ba9
 
 apt update && apt upgrade -y
 sudo apt install firefox-esr -y
