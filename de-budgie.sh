a6cb225eb2785298172f129038b82ee0cfb90745
wahasa <69626847+wahasa@users.noreply.github.com>
2022-09-23 10:59:39 +0700

    Create de-budgie.sh

---
+++ de-budgie.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#Get the necessary components
+apt-get update
+apt-get install udisks2 -y
+echo  " "  > /var/lib/dpkg/info/udisks2.postinst
+apt-mark hold udisks2
+apt-get install keyboard-configuration -y
+apt-get install tzdata -y
+apt-get install sudo -y
+apt-get install budgie-desktop gnome-session-flashback -y
+apt-get install tigervnc-standalone-server dbus-x11 -y
+apt-get --fix-broken install
+apt-get clean
+
+#Setup the necessary files
+mkdir -p ~/.vnc
+echo "#!/bin/bash
+export PULSE_SERVER=127.0.0.1
+xrdb $HOME/.Xresources
+metacity
+budgie-panel
+gnome-flashback" > ~/.vnc/xstartup
+echo "vncserver -geometry 1600x900 -name remote-desktop :1 -xstartup" > /usr/local/bin/vnc-start
+echo "vncserver -kill :1" > /usr/local/bin/vnc-stop
+clear
+chmod +x ~/.vnc/xstartup
+chmod +x /usr/local/bin/vnc-start
+chmod +x /usr/local/bin/vnc-stop
+
+echo " "
+echo "Installing browser,.."
+echo " "
+wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/Patch/browserfix.sh && chmod +x browserfix.sh && ./browserfix.sh
+wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/libreofficefix.sh && chmod +x libreofficefix.sh && ./libreofficefix.sh
+clear
+echo " "
+echo "Vnc Server address will run at 127.0.0.1:5901"
+echo " "
+echo "Start Vnc Server, run vnc-start"
+echo " "
+echo "Stop Vnc Server, run vnc-stop"
+echo " "
+
+rm browserfix.sh
+rm de-budgie.sh
+
+vnc-start
