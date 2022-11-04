419f660f0d9c381bada6bde0c080c1f78a1cb723
wahasa <69626847+wahasa@users.noreply.github.com>
2022-11-04 13:28:53 +0700

    Update de-xfce.sh

--- Desktop/de-xfce.sh
+++ Desktop/de-xfce.sh
@@ -3,11 +3,9 @@
 #Get the necessary components
 apt-get update
 apt-get install udisks2 -y
-echo " " > /var/lib/dpkg/info/udisks2.postinst                                                                                                        apt-mark hold udisks2                                                                                                                                   apt-get install keyboard-configuration -y
+echo " " > /var/lib/dpkg/info/udisks2.postinst
 apt-mark hold udisks2
-apt-get install keyboard-configuration -y
-apt-get install tzdata -y
-apt-get install sudo -y
+apt-get install sudo tzdata -y
 apt-get install xfce4 xfce4-goodies xfce4-terminal -y
 apt-get install tigervnc-standalone-server dbus-x11 -y
 apt-get --fix-broken install
@@ -19,7 +17,7 @@ echo "#!/bin/bash
 export PULSE_SERVER=127.0.0.1
 xrdb $HOME/.Xresources
 startxfce4" > ~/.vnc/xstartup
-echo "vncserver -geometry 1600x900 -name remote-desktop :1 -xstartup" > /usr/local/bin/vnc-start
+echo "vncserver -geometry 1600x900 -name remote-desktop :1" > /usr/local/bin/vnc-start
 echo "vncserver -kill :1" > /usr/local/bin/vnc-stop
 clear
 chmod +x ~/.vnc/xstartup
