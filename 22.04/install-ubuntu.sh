a038e3248025580da6d65470eeace5e6eaa3c5f6
wahasa <69626847+wahasa@users.noreply.github.com>
2022-11-04 11:40:12 +0700

    Update install-ubuntu.sh

--- 22.04/install-ubuntu.sh
+++ 22.04/install-ubuntu.sh
@@ -64,7 +64,7 @@ command+=" -b /dev"
 command+=" -b /proc"
 command+=" -b $folder/root:/dev/shm"
 ## uncomment the following line to have access to the home directory of termux
-command+=" -b /data/data/com.termux/files/home:/root"
+#command+=" -b /data/data/com.termux/files/home:/root"
 ## uncomment the following line to mount /sdcard directly to /
 command+=" -b /sdcard"
 command+=" -w /root"
