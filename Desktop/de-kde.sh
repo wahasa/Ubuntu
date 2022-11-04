616388b2163cb06e853a57adcd1f9e4776fee4f1
wahasa <69626847+wahasa@users.noreply.github.com>
2022-11-04 13:35:11 +0700

    Update de-kde.sh

--- Desktop/de-kde.sh
+++ Desktop/de-kde.sh
@@ -5,9 +5,7 @@ apt-get update
 apt-get install udisks2 -y
 echo " " > /var/lib/dpkg/info/udisks2.postinst
 apt-mark hold udisks2
-apt-get install keyboard-configuration -y
-apt-get install tzdata -y
-apt-get install sudo -y
+apt-get install sudo tzdata -y
 apt-get install kde-plasma-desktop konsole -y
 apt-get install tigervnc-standalone-server dbus-x11 -y
 apt-get --fix-broken install
@@ -19,7 +17,7 @@ echo "#!/bin/bash
 export PULSE_SERVER=127.0.0.1
 xrdb $HOME/.Xresources
 startplasma-x11" > ~/.vnc/xstartup
-echo "vncserver -geometry 1600x900 -name remote-desktop :1 -xstartup" > /usr/local/bin/vnc-start
+echo "vncserver -geometry 1600x900 -name remote-desktop :1" > /usr/local/bin/vnc-start
 echo "vncserver -kill :1" > /usr/local/bin/vnc-stop
 clear
 chmod +x ~/.vnc/xstartup
