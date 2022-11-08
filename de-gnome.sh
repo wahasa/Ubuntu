d8616a61ab98bdf96d732995940955de422121e8
wahasa <69626847+wahasa@users.noreply.github.com>
2022-11-08 12:54:24 +0700

    Update de-gnome.sh

--- de-gnome.sh
+++ de-gnome.sh
@@ -6,7 +6,7 @@ apt-get install udisks2 -y
 echo " " > /var/lib/dpkg/info/udisks2.postinst
 apt-mark hold udisks2
 apt-get install sudo tzdata -y
-apt-get install gnome gnome-session-flashback -y
+apt-get install ubuntu-gnome-desktop gnome gnome-session-flashback -y
 apt-get install tigervnc-standalone-server dbus-x11 -y
 apt-get --fix-broken install
 apt-get clean
@@ -15,14 +15,28 @@ apt-get clean
 mkdir -p ~/.vnc
 echo "#!/bin/bash
 export PULSE_SERVER=127.0.0.1
-xrdb $HOME/.Xresources
-metacity
-gnome-panel
-gnome-flashback" > ~/.vnc/xstartup
+export XKL_XMODMAP_DISABLE=1
+
+unset SESSION_MANAGER
+unset DBUS_SESSION_BUS_ADDRESS
+
+[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
+[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+
+xsetroot -solid grey
+gnome-session --builtin --session=gnome-flashback-me>
+gnome-panel &
+gnome-flashback &" > ~/.vnc/xstartup
+echo "#!/bin/sh
+rm -rf /run/dbus/pid
+dbus-daemon --system
+dbus-launch
+DISPLAY=:1 $HOME/.vnc/xstartup" > /usr/local/bin/vncstart
 echo "vncserver -geometry 1600x900 -name remote-desktop :1" > /usr/local/bin/vnc-start
 echo "vncserver -kill :1" > /usr/local/bin/vnc-stop
 clear
 chmod +x ~/.vnc/xstartup
+chmod +x /usr/local/bin/vncstart
 chmod +x /usr/local/bin/vnc-start
 chmod +x /usr/local/bin/vnc-stop
 
@@ -35,9 +49,13 @@ clear
 echo " "
 echo "Vnc Server address will run at 127.0.0.1:5901"
 echo " "
-echo "Start Vnc Server, run vnc-start"
+echo "In Termux"
+echo "Start Vnc Server, run vncstart"
+echo "Stop  Vnc Server, run vncstop"
 echo " "
-echo "Stop Vnc Server, run vnc-stop"
+echo "In Linux"
+echo "Start Vnc, run vncstart"
+echo "Exit  Vnc, run ctrl+c"
 echo " "
 
 rm browserfix.sh
