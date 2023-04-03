c89b4970d84358d898fb7a234b0f76cdea726287
wahasa <69626847+wahasa@users.noreply.github.com>
2023-04-03 13:59:05 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+wget -q https://raw.githubusercontent.com/wahasa/Ubuntu/main/Install/install -P ../usr/bin/
+chmod +x ../usr/bin/install
+  echo "Installation successful,."
+  echo ""
+      sleep 1
+  echo -n ""
+  echo -n "L"
+      sleep 1
+  echo -n "o"
+      sleep 1
+  echo -n "a"
+      sleep 1
+  echo -n "d"
+      sleep 1
+  echo -n "i"
+       sleep 1
+  echo -n "n"
+       sleep 1
+  echo -n "g"
+       sleep 1
+  echo -n ","
+       sleep 1
+  echo -n "."
+       sleep 1
+  echo -n "."
+       sleep 1
+rm ../usr/bin/install.*
+clear
+install
+rm install.sh
