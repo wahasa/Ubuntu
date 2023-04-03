b50995a3235a11f5edad4b0b08da27eb01e4c2e6
wahasa <69626847+wahasa@users.noreply.github.com>
2023-04-03 14:41:24 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,31 +1,10 @@
 #!/bin/bash
+rm ../usr/bin/Install*
 wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/Install/Install -P ../usr/bin/
 chmod +x ../usr/bin/Install
   echo ""
   echo "Installation successful,."
   echo ""
-      sleep 1
-  echo -n ""
-  echo -n "L"
-      sleep 1
-  echo -n "o"
-      sleep 1
-  echo -n "a"
-      sleep 1
-  echo -n "d"
-      sleep 1
-  echo -n "i"
-       sleep 1
-  echo -n "n"
-       sleep 1
-  echo -n "g"
-       sleep 1
-  echo -n ","
-       sleep 1
-  echo -n "."
-       sleep 1
-  echo -n "."
-       sleep 1
 rm ../usr/bin/Install.*
 clear
 Install
