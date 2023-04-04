a50e9d624e648bb3e3a480fa5ea2dcb5318fcaf5
wahasa <69626847+wahasa@users.noreply.github.com>
2023-04-04 07:55:19 +0700

    Add files via upload

---
+++ Desktop/de-xfce.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#Get the necessary components
+apt-get update
+apt-get install udisks2 -y
+echo " " > /var/lib/dpkg/info/udisks2.postinst
+apt-mark hold udisks2
+apt-get install sudo tzdata -y
+apt-get install xfce4 xfce4-goodies xfce4-terminal parole -y
+apt-get install tigervnc-standalone-server dbus-x11 -y
+apt-get --fix-broken install
+apt-get clean
+
+#Setup the necessary files
+mkdir -p ~/.vnc
+echo "#!/bin/bash
+export PULSE_SERVER=127.0.0.1
+xrdb $HOME/.Xresources
+startxfce4" > ~/.vnc/xstartup
+echo "#!/bin/sh
+export DISPLAY=:1
+export PULSE_SERVER=127.0.0.1
+rm -rf /run/dbus/dbus.pid
+dbus-launch xfce4-session" > /usr/local/bin/vncstart
+clear
+chmod +x ~/.vnc/xstartup
+chmod +x /usr/local/bin/vncstart
+chmod +x /usr/local/bin/vnc-start
+chmod +x /usr/local/bin/vnc-stop
+   clear
+   echo "vncserver -geometry 1600x900 -name remote-desktop :1" > /usr/local/bin/vnc-start
+   echo "vncserver -kill :*" > /usr/local/bin/vnc-stop
+   echo ""
+   echo "Installing Browser,.."
+   echo ""
+#Browser Fix#
+cp /etc/apt/sources.list ~/
+echo "deb http://ftp.debian.org/debian stable main contrib non-free" >> /etc/apt/sources.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 605C66F00D6C9793
+apt update
+apt install firefox-esr -y
+echo 'user_pref("media.cubeb.sandbox", false);
+user_pref("security.sandbox.content.level", 0);' > /root/.mozilla/firefox/*.default-esr/user.js
+rm -rf /etc/apt/sources.list
+mv sources.list /etc/apt/
+   clear
+   echo ""
+   echo "Vnc Server address will run at 127.0.0.1:5901"
+   echo ""
+   echo "Start Vnc Server, run vnc-start"
+   echo ""
+   echo "Stop Vnc Server, run vnc-stop"
+   echo ""
+rm de-xfce.sh
