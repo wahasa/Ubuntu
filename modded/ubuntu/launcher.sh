ed7037bf9e30c1bb3c3017e41c234509adae04d7
wahasa <69626847+wahasa@users.noreply.github.com>
2023-04-05 19:16:45 +0700

    Update launcher.sh

--- modded/ubuntu/launcher.sh
+++ modded/ubuntu/launcher.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 apt install git -y
 apt install plank rofi -y
+
+#Rofi
 git clone --depth=1 https://github.com/adi1090x/rofi.git
 cd rofi ; ./setup.sh ; cd
-
 rm ~/.config/rofi/config.rasi
-wget -q https://github.com/wahasa/Ubuntu/raw/main/modded/ubuntu/menu.png -P ~/.local/share/icons/
+wget -q https://raw.githubusercontent.com/wahasa/Ubuntu/main/modded/ubuntu/launcher/launcher.sh -P ~/.config/rofi/launchers/type-3/
+wget -q https://raw.githubusercontent.com/wahasa/Ubuntu/main/modded/ubuntu/launcher/style-1.rasi -P ~/.config/rofi/launchers/type-3/
+rm rofi
+
+#Plank
 mkdir -p ~/.config/autostart
+wget -q https://github.com/wahasa/Ubuntu/raw/main/modded/ubuntu/menu.png -P ~/.local/share/icons/
+wget -q https://raw.githubusercontent.com/wahasa/Ubuntu/main/modded/ubuntu/launcher/plank.desktop -P ~/.config/autostart/
+wget -q https://raw.githubusercontent.com/wahasa/Ubuntu/main/modded/ubuntu/launcher/rofi-launcher.desktop -P
+wget -q https://raw.githubusercontent.com/wahasa/Ubuntu/main/modded/ubuntu/launcher/plank-preferences.desktop -P
