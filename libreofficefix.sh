dec45046e581527385380791817017faf805c0a5
wahasa <69626847+wahasa@users.noreply.github.com>
2023-04-06 08:18:42 +0700

    Update libreofficefix.sh

--- libreofficefix.sh
+++ libreofficefix.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install libreoffice -y
+apt install libreoffice -y
 rm -rf /usr/lib/libreoffice/program/oosplash
 
 wget -q https://github.com/wahasa/Ubuntu/blob/main/Patch/oosplash?raw=true -O /usr/lib/libreoffice/program/oosplash
