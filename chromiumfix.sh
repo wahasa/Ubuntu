dcc9d6d14d5b26e91191c17e50cfd25a9ea4ace7
wahasa <69626847+wahasa@users.noreply.github.com>
2023-04-06 08:14:27 +0700

    Update chromiumfix.sh

--- chromiumfix.sh
+++ chromiumfix.sh
@@ -4,15 +4,11 @@ cp /etc/apt/sources.list ~/
   echo " "
   echo "Add Debian repo for Chromium installation,.."
   echo " "
-echo "deb http://ftp.debian.org/debian buster main
-deb http://ftp.debian.org/debian buster-updates main" >> /etc/apt/sources.list
-
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys DCC9EFBF77E11517
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AA8E81B4331F7F50
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 112695A0E562B32A
-
-apt update && apt upgrade -y
+echo "deb http://ftp.debian.org/debian stable main contrib non-free" >> /etc/apt/sources.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 605C66F00D6C9793
+apt update
 sudo apt install chromium -y
 
 sed -i 's/chromium %U/chromium --no-sandbox --test-type %U/g' /usr/share/applications/chromium.desktop
