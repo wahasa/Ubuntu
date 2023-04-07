fdf9aade1e7e6fb088e20c0a1c8a02e8977fad93
wahasa <69626847+wahasa@users.noreply.github.com>
2023-04-07 09:50:17 +0700

    Update de-ubuntu.sh

--- modded/de-ubuntu.sh
+++ modded/de-ubuntu.sh
@@ -45,7 +45,7 @@ DISPLAY=:1 firefox &
 sleep 10
 pkill -f firefox
 vnc-stop
-sleep 5
+sleep 2
 
 wget -O $(find ~/.mozilla/firefox -name *.default-esr)/user.js https://raw.githubusercontent.com/wahasa/Ubuntu/main/Patch/user.js
 
