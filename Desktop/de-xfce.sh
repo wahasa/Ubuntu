0d70da48aa09ef55269f62467b74c468698fb533
wahasa <69626847+wahasa@users.noreply.github.com>
2023-04-07 09:47:00 +0700

    Update de-xfce.sh

--- Desktop/de-xfce.sh
+++ Desktop/de-xfce.sh
@@ -32,12 +32,20 @@ dbus-launch xfce4-session" > /usr/local/bin/vncstart
    echo ""
 #Browser Fix#
 cp /etc/apt/sources.list ~/
+wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/Patch/passwd -P .vnc/
 echo "deb http://ftp.debian.org/debian stable main contrib non-free" >> /etc/apt/sources.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 605C66F00D6C9793
 apt update
 apt install firefox-esr -y
+vnc-start
+sleep 5
+DISPLAY=:1 firefox &
+sleep 10
+pkill -f firefox
+vnc-stop
+sleep 2
 
 wget -O $(find ~/.mozilla/firefox -name *.default-esr)/user.js https://raw.githubusercontent.com/wahasa/Ubuntu/main/Patch/user.js
 
