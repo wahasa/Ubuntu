68957f4fb25b7a63da4559f709da9b40b89a4bf6
wahasa <69626847+wahasa@users.noreply.github.com>
2023-04-09 14:51:01 +0700

    Update de-mate.sh

--- de-mate.sh
+++ de-mate.sh
@@ -30,19 +30,7 @@ dbus-launch mate-session" > /usr/local/bin/vncstart
    echo ""
    echo "Installing Browser,.."
    echo ""
-#Browser Fix#
-cp /etc/apt/sources.list ~/
-echo "deb http://ftp.debian.org/debian stable main contrib non-free" >> /etc/apt/sources.list
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 605C66F00D6C9793
-apt update
-apt install firefox-esr -y
-
-wget -O $(find ~/.mozilla/firefox -name *.default-esr)/user.js https://raw.githubusercontent.com/wahasa/Ubuntu/main/Patch/user.js
-
-rm -rf /etc/apt/sources.list
-mv sources.list /etc/apt/
+wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/Patch/browserfix.sh ; chmod +x browserfix.sh ; ./browserfix.sh
    clear
    echo ""
    echo "Vnc Server address will run at 127.0.0.1:5901"
