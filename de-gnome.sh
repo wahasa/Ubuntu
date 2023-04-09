428a2167ee77acc15aa9eb57073fbabafb6a5bfa
wahasa <69626847+wahasa@users.noreply.github.com>
2023-04-09 15:03:50 +0700

    Create de-gnome.sh

---
+++ de-gnome.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#Get the necessary components
+apt-get update
+apt-get install udisks2 -y
+echo " " > /var/lib/dpkg/info/udisks2.postinst
+apt-mark hold udisks2
+apt-get install sudo tzdata -y
+apt-get install gnome gnome-session-flashback gnome-terminal -y
+apt-get install tigervnc-standalone-server dbus-x11 -y
+apt-get --fix-broken install
+apt-get clean
+
+#Setup the necessary files
+mkdir -p ~/.vnc
+echo "#!/bin/bash
+export PULSE_SERVER=127.0.0.1
+xrdb $HOME/.Xresources
+metacity
+gnome-panel
+gnome-flashback" > ~/.vnc/xstartup
+
+echo "#!/bin/sh
+export DISPLAY=:1
+export PULSE_SERVER=127.0.0.1
+rm -rf /run/dbus/dbus.pid
+metacity &
+gnome-panel &
+gnome-flashback &" > /usr/local/bin/vncstart
+   echo "vncserver -geometry 1600x900 -name remote-desktop :1" > /usr/local/bin/vnc-start
+   echo "vncserver -kill :*" > /usr/local/bin/vnc-stop
+   chmod +x ~/.vnc/xstartup
+   chmod +x /usr/local/bin/*
+   clear
+   echo ""
+   echo "Installing Browser,.."
+   echo ""
+wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/Patch/browserfix.sh ; chmod +x browserfix.sh ; ./browserfix.sh
+wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/libreofficefix.sh ; chmod +x libreofficefix.sh ; ./libreofficefix.sh
+   clear
+   echo ""
+   echo "In Termux"
+   echo "Start Vnc Server, run vncstart"
+   echo "Stop  Vnc Server, run vncstop"
+   echo ""
+   echo "In Linux"
+   echo "Start Vnc, run vncstart"
+   echo "Exit  Vnc, run ctrl+c"
+   echo ""
+rm de-gnome.sh
