fa8f9f8c9e05716cb49ae7b2fb45455321181fff
wahasa <69626847+wahasa@users.noreply.github.com>
2023-04-17 04:14:03 +0700

    Update vscodefix.sh

--- Apps/vscodefix.sh
+++ Apps/vscodefix.sh
@@ -14,7 +14,7 @@ case `uname -m` in
       wget "https://az764295.vo.msecnd.net/stable/704ed70d4fd1c6bd6342c436f1ede30d1cff4710/code_1.77.3-1681291917_${archurl}.deb"
       wget "https://az764295.vo.msecnd.net/stable/704ed70d4fd1c6bd6342c436f1ede30d1cff4710/code_1.77.3-1681295476_${archurl}.deb"
       wget "https://az764295.vo.msecnd.net/stable/704ed70d4fd1c6bd6342c436f1ede30d1cff4710/code_1.77.3-1681292746_${archurl}.deb"
-sheep 1
+sleep 1
 apt install ~/code*
 rm /usr/share/applications/code.desktop
 
