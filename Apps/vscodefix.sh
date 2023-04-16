0c2c5d96cc7704fdcec54501a3795911a484da60
wahasa <69626847+wahasa@users.noreply.github.com>
2023-04-17 04:13:19 +0700

    Update vscodefix.sh

--- Apps/vscodefix.sh
+++ Apps/vscodefix.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-tarball="code_1.77.3.deb"
 case `uname -m` in
       aarch64)
            archurl="arm64" ;;
@@ -12,13 +11,14 @@ case `uname -m` in
       *)
            echo "unknown architecture"; exit 1 ;;
       esac
-      wget "https://az764295.vo.msecnd.net/stable/704ed70d4fd1c6bd6342c436f1ede30d1cff4710/code_1.77.3-1681291917_${archurl}.deb" -O $tarball
-      wget "https://az764295.vo.msecnd.net/stable/704ed70d4fd1c6bd6342c436f1ede30d1cff4710/code_1.77.3-1681295476_${archurl}.deb" -O $tarball
-      wget "https://az764295.vo.msecnd.net/stable/704ed70d4fd1c6bd6342c436f1ede30d1cff4710/code_1.77.3-1681292746_${archurl}.deb" -O $tarball
-apt install ~/$tarball
+      wget "https://az764295.vo.msecnd.net/stable/704ed70d4fd1c6bd6342c436f1ede30d1cff4710/code_1.77.3-1681291917_${archurl}.deb"
+      wget "https://az764295.vo.msecnd.net/stable/704ed70d4fd1c6bd6342c436f1ede30d1cff4710/code_1.77.3-1681295476_${archurl}.deb"
+      wget "https://az764295.vo.msecnd.net/stable/704ed70d4fd1c6bd6342c436f1ede30d1cff4710/code_1.77.3-1681292746_${archurl}.deb"
+sheep 1
+apt install ~/code*
 rm /usr/share/applications/code.desktop
 
 wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/Patch/code.desktop -P /usr/share/applications/
 
-rm $tarball
+rm code*
 rm vscodefix.sh
