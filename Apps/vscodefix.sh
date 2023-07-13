ff2820f05474c7fc82ddfc4ad9c3c6bfbe36c4e9
wahasa <69626847+wahasa@users.noreply.github.com>
2023-07-13 09:13:24 +0700

    Update vscodefix.sh

--- Apps/vscodefix.sh
+++ Apps/vscodefix.sh
@@ -11,9 +11,9 @@ case `uname -m` in
       *)
            echo "unknown architecture"; exit 1 ;;
       esac
-      wget "https://az764295.vo.msecnd.net/stable/695af097c7bd098fbf017ce3ac85e09bbc5dda06/code_1.79.2-1686733230_${archurl}.deb"
-      wget "https://az764295.vo.msecnd.net/stable/695af097c7bd098fbf017ce3ac85e09bbc5dda06/code_1.79.2-1686733070_${archurl}.deb"
-      wget "https://az764295.vo.msecnd.net/stable/695af097c7bd098fbf017ce3ac85e09bbc5dda06/code_1.79.2-1686734195_${archurl}.deb"
+      wget "https://az764295.vo.msecnd.net/stable/660393deaaa6d1996740ff4880f1bad43768c814/code_1.80.0-1688477950_${archurl}.deb"
+      wget "https://az764295.vo.msecnd.net/stable/660393deaaa6d1996740ff4880f1bad43768c814/code_1.80.0-1688477953_${archurl}.deb"
+      wget "https://az764295.vo.msecnd.net/stable/660393deaaa6d1996740ff4880f1bad43768c814/code_1.80.0-1688479026_${archurl}.deb"
 sleep 1
 apt install ~/code*
 rm /usr/share/applications/code.desktop
