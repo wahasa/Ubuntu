933117c058ca3166ab5368fcf558a14f742d1bda
wahasa <69626847+wahasa@users.noreply.github.com>
2024-01-28 14:06:39 +0700

    chromiumfix.sh

--- Apps/chromiumfix.sh
+++ Apps/chromiumfix.sh
@@ -8,8 +8,7 @@ echo "deb http://ftp.debian.org/debian stable main contrib non-free" >> /etc/apt
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 605C66F00D6C9793
-apt update
-apt install chromium -y
+apt update ; apt install chromium -y
 
 sed -i 's/chromium %U/chromium --no-sandbox --test-type %U/g' /usr/share/applications/chromium.desktop
 
