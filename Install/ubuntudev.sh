7d05c80f04b8b0d134e413b14554d08d30218a2b
wahasa <69626847+wahasa@users.noreply.github.com>
2024-07-19 08:16:14 +0700

    ubuntudev.sh

--- Install/ubuntudev.sh
+++ Install/ubuntudev.sh
@@ -1,13 +1,12 @@
 #!/data/data/com.termux/files/usr/bin/bash
 pkg install root-repo x11-repo
-pkg install proot pulseaudio -y
+pkg install proot xz-utils pulseaudio -y
 termux-setup-storage
 ubuntu=noble
-#ubuntu=mantic
 folder=ubuntu-fs
 if [ -d "$folder" ]; then
         first=1
-        echo "skipping downloading"
+        echo "Skipping Downloading"
 fi
 tarball="ubuntu-rootfs.tar.gz"
 if [ "$first" != 1 ];then
@@ -23,7 +22,7 @@ if [ "$first" != 1 ];then
                 x86_64)
                         archurl="amd64" ;;
                 *)
-                        echo "unknown architecture"; exit 1 ;;
+                        echo "Unknown Architecture"; exit 1 ;;
                 esac
                 wget "https://cdimage.ubuntu.com/ubuntu-base/daily/current/${ubuntu}-base-${archurl}.tar.gz" -O $tarball
                 #wget "https://partner-images.canonical.com/oci/${ubuntu}/current/ubuntu-${ubuntu}-oci-${archurl}-root.tar.gz" -O $tarball
@@ -48,7 +47,7 @@ pulseaudio --start \
     --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" \
     --exit-idle-time=-1
 cd \$(dirname \$0)
-## unset LD_PRELOAD in case termux-exec is installed
+## Unset LD_PRELOAD in case termux-exec is installed
 unset LD_PRELOAD
 command="proot"
 command+=" --kill-on-exit"
@@ -65,9 +64,9 @@ command+=" -b /dev/null:/proc/sys/kernel/cap_last_cap"
 command+=" -b /proc"
 command+=" -b /data/data/com.termux/files/usr/tmp:/tmp"
 command+=" -b $folder/root:/dev/shm"
-## uncomment the following line to have access to the home directory of termux
+## Uncomment the following line to have access to the home directory of termux
 #command+=" -b /data/data/com.termux/files/home:/root"
-## uncomment the following line to mount /sdcard directly to /
+## Uncomment the following line to mount /sdcard directly to /
 command+=" -b /sdcard"
 command+=" -w /root"
 command+=" /usr/bin/env -i"
@@ -88,10 +87,7 @@ EOM
    #Making $linux executable"
    chmod +x $bin
    #Removing image for some space"
-   rm $tarball
-echo '#!/bin/bash
-bash .ubuntu' > $PREFIX/bin/$linux
-chmod +x $PREFIX/bin/$linux
+   #rm $tarball
 #Repositories
 echo "#Ubuntu Development
 deb http://ports.ubuntu.com/ubuntu-ports devel main restricted universe multiverse
@@ -99,6 +95,9 @@ deb http://ports.ubuntu.com/ubuntu-ports devel-updates main restricted universe 
 deb http://ports.ubuntu.com/ubuntu-ports devel-security main restricted universe multiverse
 deb http://ports.ubuntu.com/ubuntu-ports devel-proposed main restricted universe multiverse
 deb http://ports.ubuntu.com/ubuntu-ports devel-backports main restricted universe multiverse" > ~/"$folder"/etc/apt/sources.list
+echo "export PULSE_SERVER=127.0.0.1" >> $folder/root/.bashrc
+echo 'bash .ubuntu' > $PREFIX/bin/$linux
+chmod +x $PREFIX/bin/$linux
    clear
    echo ""
    echo "Updating Ubuntu,.."
@@ -113,6 +112,10 @@ exit" > $folder/root/.bash_profile
 bash $linux
    clear
    echo ""
-   echo "You can now start Ubuntu with 'ubuntu' script next time"
+   echo "You can login to Ubuntu with 'ubuntu' script next time"
    echo ""
-#rm ubuntu.sh
+   #rm ubuntudev.sh
+
+#
+## Script edited by 'WaHaSa', Script V3-revision.
+#
