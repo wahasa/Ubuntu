c5f69f7a6a82da7d3f1a94f986e808b4ba495494
wahasa <69626847+wahasa@users.noreply.github.com>
2025-12-15 20:03:46 +0700

    Update firefoxfix.sh

--- Apps/firefoxfix.sh
+++ Apps/firefoxfix.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+echo ""
+echo "Add Debian repo for Firefox installation,.."
+echo ""
+
 mv /etc/apt/sources.list /etc/apt/sources.list.bak
-  echo " "
-  echo "Add Debian repo for Firefox installation,.."
-  echo " "
-
-wget https://ftp.debian.org/debian/pool/main/d/debian-archive-keyring/debian-archive-keyring_2025.1_all.deb
 wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/Patch/passwd -P .vnc/
+wget https://ftp.debian.org/debian/pool/main/d/debian-archive-keyring/debian-archive-keyring_2025.1_all.deb
 
-echo "deb http://ftp.debian.org/debian stable main" > /etc/apt/sources.list
 sudo apt install ~/debian-archive-keyring_2025.1_all.deb
+#echo "deb http://ftp.debian.org/debian stable main" > /etc/apt/sources.list
+echo "#Modernized from /etc/apt/sources.list
+Types: deb
+URIs: http://ftp.debian.org/debian
+Suites: stable
+Components: main
+Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg" > /etc/apt/sources.list.d/firefox.sources
 
 apt update ; sudo apt install firefox-esr -y
 
@@ -22,6 +28,5 @@ sleep 2
 
 wget -O $(find ~/.mozilla/firefox -name *.default-esr)/user.js https://raw.githubusercontent.com/wahasa/Ubuntu/main/Patch/user.js
 
-echo"" > /etc/apt/sources.list
-rm .vnc/passwd
-rm firefoxfix.sh
+#mv /etc/apt/sources.list.bak /etc/apt/sources.list
+rm .vnc/passwd ; rm 
