b906282bed2083036a8da695cc76f9f22395b77d
wahasa <69626847+wahasa@users.noreply.github.com>
2025-12-15 15:13:41 +0700

    Update de-xfce.sh

--- Desktop/de-xfce.sh
+++ Desktop/de-xfce.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #Get the necessary components
 sudo apt update
-sudo apt install udisks2 elementary-xfce-icon-theme -y
+sudo apt install udisks2 -y
 echo "" > /var/lib/dpkg/info/udisks2.postinst
 sudo apt-mark hold udisks2
+sudo apt install software-properties-common elementary-xfce-icon-theme -y
 sudo apt install xfce4 xfce4-goodies xfce4-appmenu-plugin parole -y
 sudo apt install tigervnc-standalone-server dbus-x11 -y
 sudo apt --fix-broken install
@@ -13,7 +14,7 @@ sudo apt clean
 mkdir -p ~/.vnc
 echo "#!/bin/bash
 export PULSE_SERVER=127.0.0.1
-xrdb $HOME/.Xresources
+#service dbus start
 startxfce4" > ~/.vnc/xstartup
 
 echo "#!/bin/sh
@@ -21,10 +22,9 @@ export DISPLAY=:1
 export PULSE_SERVER=127.0.0.1
 rm -rf /run/dbus/dbus.pid
 dbus-launch startxfce4" > /usr/local/bin/vncstart
-   echo "vncserver -geometry 1600x900 -name remote-desktop :1" > /usr/local/bin/vnc-start
+   echo "vncserver -geometry 1600x900 remote-desktop :1 ; pkill vncconfig" > /usr/local/bin/vnc-start
    echo "vncserver -kill :* ; rm -rf /tmp/.X*-lock ; rm -rf /tmp/.X11-unix/X* ; rm -rf ~/.vnc/localhost:*.pid" > /usr/local/bin/vnc-stop
-   chmod +x ~/.vnc/xstartup
-   chmod +x /usr/local/bin/*
+   chmod +x ~/.vnc/xstartup ; chmod +x /usr/local/bin/*
    clear
    echo ""
    echo "Installing Browser,.."
