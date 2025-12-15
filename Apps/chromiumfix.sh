af70482d0b0e72b7648b61742978ec7fac7c65c5
wahasa <69626847+wahasa@users.noreply.github.com>
2025-12-15 20:39:56 +0700

    Update chromiumfix.sh

--- Apps/chromiumfix.sh
+++ Apps/chromiumfix.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-clear
-cp /etc/apt/sources.list ~/
-  echo " "
-  echo "Add Debian repo for Chromium installation,.."
-  echo " "
-echo "deb http://ftp.debian.org/debian stable main contrib non-free" >> /etc/apt/sources.list
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 605C66F00D6C9793
-apt update ; apt install chromium -y
+echo ""
+echo "Add Mozilla repo for Firefox installation,.."
+#echo "Add Mozilla repo for Thunderbird installation,.."
+echo ""
 
-sed -i 's/chromium %U/chromium --no-sandbox --test-type %U/g' /usr/share/applications/chromium.desktop
+#sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:mozillateam/ppa
+sudo apt update
 
-rm -rf /etc/apt/sources.list
-mv sources.list /etc/apt/
+#Firefox Browser
+sudo apt install firefox-esr -y 
 
-rm chromiumfix.sh
+#Thunderbird
+#sudo apt install thunderbird -y
+
+rm
